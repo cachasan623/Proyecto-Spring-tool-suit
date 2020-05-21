aa0ee65b0eeb5d255ff797ac84136c8daec928c8
cachasan623 <55565932+cachasan623@users.noreply.github.com>
2020-05-21 16:34:01 -0300

    Asunto: Inicializacion del Proyecto Responsable: Nahuel Alberto Cachambi Task00: Inicializacion del Proyecto

---
+++ src/main/java/ar/edu/unju/fi/Cachambi3829Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cachambi3829Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cachambi3829Tp5Application.class, args);
+	}
+
+}
